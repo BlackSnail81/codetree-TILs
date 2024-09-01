a8529c1138babe915adf8d3ce550a4208159542b
BlackSnail81 <125931487+BlackSnail81@users.noreply.github.com>
2024-09-01 21:03:14 +0900

    Title: 세 수의 합과 평균 구하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240901/세 수의 합과 평균 구하기 2/sum-and-mean-of-three-numbers-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int x,y,z;
+    cin >> x >> y >> z;
+
+    int avg, sum;
+    avg = (x + y + z)/3;
+    sum = (x + y + z);
+
+    cout << "avg " << avg << endl;
+    cout << "sum " << sum;
+
+    
+
+    return 0;
+}
