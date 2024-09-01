7bd205d4e6f7274769dffe49f87ef7ab29cdeffa
BlackSnail81 <125931487+BlackSnail81@users.noreply.github.com>
2024-09-01 21:06:27 +0900

    Title: 입력받아 연산하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240901/입력받아 연산하기 2/take-input-and-operate-2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    int res1, res2;
+
+    cin >> a >> b;
+    
+    res1 = a + 894;
+    res2 = b / 10;
+
+
+    cout << res1 << endl;
+    cout << res2;
+
+    return 0;
+}
