82e8734a54853af0fdd6ac4155ebdeedbdad9b59
BlackSnail81 <125931487+BlackSnail81@users.noreply.github.com>
2024-09-07 17:08:31 +0900

    Title: 함수 실행 횟수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240907/함수 실행 횟수/number-of-function-executions.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int codetree(int n){
+    int cnt = 0;
+    cnt++;
+    if (n < 2){        
+        return cnt;        
+    } else {
+        cnt += 2;
+        codetree(n-1) + codetree(n-2);
+        return cnt;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int res, n;
+    cin >> n;
+
+    res = codetree(n);
+
+    //if(n < 2) res = 1;
+    cout << res;
+
+
+    return 0;
+}
