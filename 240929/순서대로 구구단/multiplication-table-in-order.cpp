1bb95330214aac7636893f8ae969780d840da685
BlackSnail81 <125931487+BlackSnail81@users.noreply.github.com>
2024-09-29 21:57:45 +0900

    Title: 순서대로 구구단, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240929/순서대로 구구단/multiplication-table-in-order.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b;
+    cin >> a >> b;
+
+    for(int i = 1; i <= 9; i++){
+        for(int j = a; j <= b; j++){
+            cout << j << " * " << i << " = " << j*i << "  ";
+        }
+        cout << endl;
+    }
+    
+    return 0;
+}
