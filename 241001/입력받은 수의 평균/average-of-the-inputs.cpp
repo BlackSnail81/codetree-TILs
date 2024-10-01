92c4e2da62834714e64df930fc182877a8cc88cd
BlackSnail81 <125931487+BlackSnail81@users.noreply.github.com>
2024-10-01 15:50:30 +0900

    Title: 입력받은 수의 평균, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241001/입력받은 수의 평균/average-of-the-inputs.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+
+    int input;
+    double sum = 0;
+    double avg = 0.0;
+
+    for(int i = 0; i < n; i++){
+        cin >> input;
+        sum += input;
+    }
+
+    avg = sum / n;
+
+    cout << fixed;
+    cout.precision(1);
+
+    if(avg >= 70){
+    cout << avg;
+    }else
+    cout << "fail";
+
+    return 0;
+}
