dca41ee93b0fba6b16b3ab79f3395764a9978115
BlackSnail81 <125931487+BlackSnail81@users.noreply.github.com>
2024-10-01 17:50:22 +0900

    Title: 영문 소문자의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241001/영문 소문자의 개수/number-of-lowercase-letters.cpp
+++ 241001/영문 소문자의 개수/number-of-lowercase-letters.cpp
@@ -12,13 +12,8 @@ using namespace std;
 int main() {
 
 	std::ios::sync_with_stdio(false);
-	unordered_map<char, int> um;
+	unordered_map<char, int> um;	
 	
-	/*
-	if (um.empty()) {
-		cout << "unordered_map은 비어있습니다" << endl;
-	}*/
-
 	int n;
 	cin >> n;
 
@@ -26,16 +21,14 @@ int main() {
 		char s;
 		cin >> s;
 
-		//대문자 판별, 소문자 판별은 islower
-		if (isupper(s)) break;
+		//대문자 판별 isupper, 소문자 판별 islower, 숫자 판별 isdigit
+		if (isupper(s)||isdigit(s)) break;
 
 		if (um.find(s) != um.end()) {
 			um[s]++;  //해당key값의 value만 1증가
 		}
 		um.insert(make_pair(s, 1));
-	}
-	
-	//cout << "unordered_map의 크기는 " << um.size() << " 입니다" << endl;
+	}	
 
 	vector<pair<char, int>> sortingArray(um.begin(), um.end()); //정렬을 위한 벡터
 	sort(sortingArray.begin(), sortingArray.end(),
