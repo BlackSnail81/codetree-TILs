a603a2fe9feb45083152035b6ef5e6e4ffcca8b8
BlackSnail81 <125931487+BlackSnail81@users.noreply.github.com>
2024-10-06 22:15:27 +0900

    Title: 럭키 세븐, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241006/럭키 세븐/lucky-seven.cpp
+++ 241006/럭키 세븐/lucky-seven.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n;
+    int n, input;
     int cnt, sum;
     double avg;
 
@@ -15,11 +15,11 @@ int main() {
     cin >> n;
 
     for(int i = 0; i < n; i++){
-        cin >> n;
+        cin >> input;
 
         if(n%7==0){
             cnt++;
-            sum=sum+n;
+            sum=sum+input;
         }
 
     }
